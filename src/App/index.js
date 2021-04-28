fae6af493242fc8d60c3bd57e078543946390823
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-04-28 14:17:57 -0500

    working on api call

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
-import WeatherCard from '../Components/WeatherCard';
+import React, { useEffect } from 'react';
+import WeatherForm from '../Components/form';
+// import WeatherCard from '../Components/WeatherCard';
 import getWeather from '../helpers/data/weatherData';
 import './App.scss';
 
 function App() {
-  const [weather, setWeather]= useState({});
-  
+  // const [weather, setWeather]= useState({});
   useEffect(() => {
     getWeather().then((weatherObj) => console.warn(weatherObj));
   }, []);
 
   return (
     <div className='App'>
-      <WeatherCard
+      <WeatherForm />
+      {/* <WeatherCard
       weather={weather}
       setWeather={setWeather}
-    />
+    /> */}
     </div>
   );
 }
--- src/Components/form.js
+++ src/Components/form.js
@@ -1,13 +1,16 @@
-import React from 'React';
+import React, { useState } from 'react';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button
+} from 'reactstrap';
+import getWeather from '../helpers/data/weatherData';
 
-export default function weatherForm() {
-  return (
-    <Form>
-    <FormGroup>
-      <Label for="location">Enter Your Location</Label>
-      <Input type="text" name="location" id="location" placeholder="Location" />
-    </FormGroup>
-    <Button color="info" type="submit">Submit</Button>
-    </Form>
-  );
-}
+export default function WeatherForm() {
+  const [userLocation, setUserLocation] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    g
