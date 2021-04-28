7ef12868e3f6ac97a9e25d39df7df9d27fe71fb6
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-04-28 18:42:17 -0500

    fixing untracked files problem

--- src/Components/form.js
+++ src/Components/form.js
@@ -8,9 +8,27 @@ import {
 } from 'reactstrap';
 import getWeather from '../helpers/data/weatherData';
 
-export default function WeatherForm() {
-  const [userLocation, setUserLocation] = useState('');
+export default function weatherForm() {
+  const [userLocation, setUserLocation] = useState({});
+  const handleInputChange = (e) => {
+    setUserLocation((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    g
+    getWeather(userLocation.location).then((resp) => console.warn(resp));
+  };
+
+  return (
+    <Form>
+    <FormGroup>
+      <Label for="location">Enter Your Location</Label>
+      <Input type="text" name="location" id="location" placeholder="Location" onChange={handleInputChange}/>
+    </FormGroup>
+    <Button color="info" type="submit" onClick={handleSubmit}>Submit</Button>
+    </Form>
+  );
+}
